d82aa3dfdf9032a34da67c2d3da2210e96df0ccc
Khurshid Alam <khurshid1556@gmail.com>
2025-09-16 18:35:26 +0530

    img

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="w-full min-h-screen relative  bg-cover bg-center bg-no-repeat font-mono ">
       <img
-        src="public/imgi_1_bg-body.png"
+        src="/public/imgi_1_bg-body.png"
         alt=""
         className="fixed top-0 left-0 -z-[1] w-full h-[100vh] object-cover object-center"
       />
